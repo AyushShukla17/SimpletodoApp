acff96b270a8b0be084d604d2f70e4b7ea174d42
Ayush Shukla <ayush.14bit1061@abes.ac.in>
2018-05-06 00:29:31 +0530

    Fixed Adding Empty Todos

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// let todoarray = [];
-// let donearray = [];
-
 
 $('.todolist').on('click','.deletebutton',function(){
 	$(this).parent().parent().remove();
@@ -61,8 +58,6 @@ $('.donelist').on('change','.mycheckbox',function(){
 $('.addbutton').on('click',function(){
 	let todo = $('input').val();
 	if(todo!="" && todo != " "){
-		// todoarray.push(todo)
-		// localStorage.setItem(');
 		$('input').val(" ");
 	let str = '<li>'+ '<input type="checkbox" class = "mycheckbox">' +'<span class="todoitem">' + todo +'</span>'+ ' <input type="text" id="edit" style="display:none" /> '  + '<span class="buttonspan">  <span class="editbutton">EDIT</span> ' + '  <span style="display:none;" class="savebutton">SAVE</span>' + "  " +'<span class="deletebutton">DELETE</span></span><hr style="opacity:0.5">'
 	$('.todolist').append(str);
