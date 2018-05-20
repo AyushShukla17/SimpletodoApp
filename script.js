9e4169b2df7440ad37e0295c0b70354aa5280f06
Ayush Shukla <ayush.14bit1061@abes.ac.in>
2018-05-20 17:46:01 +0530

    Now Add Todo's on the click of Enter Button

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let input = document.getElementById("myInput");
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("myBtn").click();
+    }
+});
+
+
 $('.todolist, .donelist').on('click','.deletebutton',function(){
 	$(this).parent().parent().remove();
 })
